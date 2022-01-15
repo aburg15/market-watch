293d50b2a7c80f9156696c7822254a9a5ddaa9e8
Adam Burgess <aburg1515@gmail.com>
2022-01-15 10:57:02 -0700

    Add basic grid with fetched data

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,10 +1,18 @@
 import '../css/Stock.css';
 
-const Stock = () => {
+const Stock = ({ id, name, image, price, market_cap, percent_change }) => {
   return (
-    <div className="stock">
-      
-    </div>
+    <section className="stock">
+      <img className='stock-img' src={image} alt='stock-image' />
+      <div className='stock-details'>
+        <div>
+          <h2 className='stock-title'>{name}</h2>
+        </div>
+        <p className='stock-price'>${price.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+        <p>{percent_change}</p>
+        <p>{market_cap}</p>
+      </div>
+    </section>
   );
 }
 
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,10 +1,25 @@
 import '../css/StockContainer.css';
+import Stock from './Stock';
 
 
-const StockContainer = () => {
+const StockContainer = ({ stocks }) => {
+  const stockCards = stocks.map((stock, index) => {
+    return (
+      <Stock 
+        id={stock.id}
+        name={stock.name}
+        image={stock.image}
+        price={stock.current_price}
+        market_cap={stock.market_cap}
+        percent_change={stock.price_change_percentage_24h}
+        key={index}
+      />
+
+    )
+  })
   return (
     <div className="stock-container">
-      
+      {stockCards}
     </div>
   );
 }
